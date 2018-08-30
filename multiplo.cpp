52b301dea571a02534f847202748afed75897df1
mramos26 <42850414+mramos26@users.noreply.github.com>
2018-08-30 16:14:44 -0500

    Update multiplo.cpp

--- multiplo.cpp
+++ multiplo.cpp
@@ -1,33 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
-
-
 using namespace std;
-
-
 int main()
-
 {
-   
-	int num1, num2;
-   
-   
+  
+int num1, num2;
+  
 	cout<<"dame primer numero";
-   
 cin>> num1;
-   
-    
 cout<< "dame segundo numero"; 
-   
 cin>> num2;
-    
-    
-if(num1 % num2 == 0)  {
+if(num2 % num1 == 0)  {
         
-cout<< "el numero "<< num2 << "es multiplo de " << num1 <<;
+cout<< "el numero "<< num2 << "es multiplo de " << num1 ;
     
 } else {
         
-cout<< "el numero "<< num2 << "no es multiplo de " << num1 <<;
+cout<< "el numero "<< num2 << "no es multiplo de " << num1 ;
    
  } 
        
