2727d7673898a0c17890805ae47786591f5f9d4c
mramos26 <42850414+mramos26@users.noreply.github.com>
2018-08-30 16:11:33 -0500

    Create multiplo.cpp

---
+++ multiplo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+   
+	int num1, num2;
+   
+   
+	cout<<"dame primer numero";
+   
+cin>> num1;
+   
+    
+cout<< "dame segundo numero"; 
+   
+cin>> num2;
+    
+    
+if(num1 % num2 == 0)  {
+        
+cout<< "el numero "<< num2 << "es multiplo de " << num1 <<;
+    
+} else {
+        
+cout<< "el numero "<< num2 << "no es multiplo de " << num1 <<;
+   
+ } 
+       
+
+return 0;
+
+}
